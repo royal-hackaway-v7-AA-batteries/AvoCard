b0551208fc8cc49842ae071fc9c06c2e87d8a3a3
Binbinbubin <cqbinbinbin@gmail.com>
2024-01-28 15:55:11 +0800

    ActionButton2

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,15 +1,20 @@
+import Divider from "./Divider";
+
 interface props {
   content: string[];
 }
 
 const ActionButton = ({ content = [""] }: props) => {
   return (
-    <div className=" pointer-events-none absolute left-0 top-0 flex h-full w-full items-end justify-center ">
-      <div className=" pointer-events-auto flex gap-1 rounded-full bg-zinc-900 text-gray-100 ">
-        {content.map((i) => {
+    <div className=" pointer-events-none absolute left-0 top-0 flex h-full w-full items-end justify-center p-4 ">
+      <div className=" pointer-events-auto flex w-full justify-around gap-1 rounded-full bg-zinc-900 text-gray-100 ">
+        {content.map((i, index) => {
           return (
             <>
-              <div className=" px-6 py-2 ">{i}</div>
+              <div className=" flex items-center ">
+                <div className=" px-6 py-2 ">{i}</div>
+                {index < content.length - 1 && <Divider />}
+              </div>
             </>
           );
         })}
---
+++ src/components/Divider.tsx
@@ -0,0 +1,5 @@
+const Divider = () => {
+  return <div className=" h-2/3 w-px bg-gray-100 " />;
+};
+
+export default Divider;
